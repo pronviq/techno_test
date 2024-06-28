082f06ec112af1b39608603df234b0791a17d21b
me <me>
2024-06-28 14:45:56 +0700

    v0.0.1 start

---
+++ src/stories/Button.stories.ts
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+import Button from "../components/Button";
+
+const meta = {
+  title: "Example/Button",
+  component: Button,
+  parameters: {
+    layout: "centered",
+  },
+  // tags: ["autodocs"],
+  argTypes: {
+    backgroundColor: { control: "color" },
+  },
+  args: { onClick: fn() },
+} satisfies Meta<typeof Button>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Large: Story = {
+  args: {
+    size: "large",
+    label: "Большая кнопочка",
+  },
+};
+
+export const Small: Story = {
+  args: {
+    size: "small",
+    label: "Маленькая кнопка",
+  },
+};
